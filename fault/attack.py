6a3a6b611c2950ef299395962f5fb5cf895e5e14
bumblebot1 <rm14834@my.bristol.ac.uk>
2017-04-01 00:15:15 +0100

    better lookup table and some more of the attack

--- fault/attack.py
+++ fault/attack.py
@@ -15,14 +15,25 @@ def Interact(fault, message):
   target_in.write("%X\n" % message)
   target_in.flush()
 
-  m = target_out.readline()
-  x = [int(m[i:i+2], 16) for i in range(0, len(m) -1, 2)]
-  return int(m, 16), x
+  c = target_out.readline()
+  x = [int(c[i:i+2], 16) for i in range(0, len(c) - 1, 2)]
+  return int(c, 16), x
 
 def StepOne(x, x_faulty):
+  equations = [[[] for a in range(256)] for b in range(16)]
   for i in range(16):
+    if i in [0, 2, 9, 11]:
+      ind = "2"
+    elif i in [5, 7, 12, 14]:
+      ind = "3"
+    else:
+      ind = "1"
+    ### find all k in 0 -> 256 such that the equations hold
     for k in range(256):
       d_i = SubBytesInverse[x[i] ^ k] ^ SubBytesInverse[x_faulty[i] ^ k]
+      for cnt, d in enumerate(MultiplicationTables[ind]):
+        if d_i == d:
+          equations[i][cnt].append(k)
 
 m = random.getrandbits(128)
 
