5c7bdf53c32ba28ea3184ec17ac6144d47d5fb72
bumblebot1 <rm14834@my.bristol.ac.uk>
2017-05-05 14:15:08 +0100

    first part of recovery works I think

--- power/attack.hpp
+++ power/attack.hpp
@@ -14,6 +14,8 @@ using namespace std;
 /*
  * @brief Constants to describe the relevant error codes returned by the target.
  */
+#define minSample 20
+#define traceSize 5000
 
 class Attack {
   private:
@@ -21,19 +23,26 @@ class Attack {
     FILE* target_out;
     unsigned long interactionCount;
     vector<mpz_class> messages;
+    vector<mpz_class> sectorsPhase2;
     vector<mpz_class> messagesPhase2;
+    vector<mpz_class> tweaks;
     vector<vector<int>>powerTraces;
     vector<vector<int>>powerTracesPhase2;
+    gmp_randclass randomGenerator{gmp_randinit_default};
+    mpz_class maxSector = 16777216;
     int sampleCount;
+    int sampleCountPhase2;
     void (*cleanup)(int s);
   
   private:
     void AddMoreSamples();
+    void SamplePart2();
+    bool TestKey(vector<unsigned char> key1, vector<unsigned char> key2);
     void Interact(int block, mpz_class sector, mpz_class& message, vector<int>& power, vector<int>& powerPhase2);
     float Mean(vector<int> trace);
     vector<unsigned char> Phase1();
     vector<unsigned char> Phase2();
-    float PearsonCorr(vector<int> x, vector<int> y);
+    double PearsonCorr(vector<int> x, vector<int> y);
     void throwErrorAndAbort(string errorMessage);
   
   public:
@@ -41,193 +50,169 @@ class Attack {
     void Execute();
 };
 
-/**
- * @brief Constructor for the Attack Class.
- *
- * @param in     pointer to the stdin of the target.
- * @param out    pointer to the stdout of the target.
- * @param clean  the cleanup function to be invoked on abnormal exit.
- */
 Attack::Attack(FILE* in, FILE* out, void (*clean)(int s)) {
   interactionCount = 0;
-  sampleCount = 0;
+  sampleCount      = 0;
 
-  target_in  = in;
-  target_out = out;
-  cleanup = clean;
+  target_in        = in;
+  target_out       = out;
+  cleanup          = clean;
 }
 
-/**
- * @brief Function to interact with the attack target
- *
- * @param block    integer value representing the block address
- * @param sector   octet string value representing the sector address or XTS-AES tweak
- * @param message  mpz_class value which will be set to the message obtained from the target Driver
- * @param power    the first 10% of the power trace for the given block and sector
- */
 void Attack::Interact(int block, mpz_class sector, mpz_class& message, vector<int>& power, vector<int>& powerPhase2) {
   gmp_fprintf(target_in, "%d\n", block);  
-  gmp_fprintf(target_in, "%032ZX\n", sector.get_mpz_t());
+  unsigned char m_char[16] = {0};
+  mpz_export(m_char, NULL, 1, 1, 0, 0, sector.get_mpz_t());
+  for(int i = 0; i < 16; i++){
+   gmp_fprintf(target_in, "%02X", m_char[i]);
+  }
+  gmp_fprintf(target_in, "\n");
   fflush(target_in);
 
   int length;
   gmp_fscanf(target_out, "%d", &length);
-  power.resize(length / 10);
-  powerPhase2.resize(length / 10);
-  int ind = 0;
-  for(; ind < length / 10; ind++){
-    gmp_fscanf(target_out, ",%d", &power[ind]);
-  }
-  while(ind < length/2) {
-    int val;
+  power.resize(traceSize);
+  powerPhase2.resize(traceSize);
+  int ind;
+  int val;
+  for(ind = 0; ind < length / 2; ind++){
     gmp_fscanf(target_out, ",%d", &val);
-    ind++;
+    if(ind < traceSize){
+      power[ind] = val;
+    }
   }
   int k = 0;
-  for(; ind < length/2 + length/10; ind++){
-    gmp_fscanf(target_out, ",%d", &powerPhase2[k++]);
+  for(; ind < length; ind++){
+    gmp_fscanf(target_out, ",%d", &val);
+    if(ind >= length - traceSize){
+      powerPhase2[k++] = val;
+    }
   }
   
-  gmp_fscanf(target_out, "%*[^\n]");
-  gmp_fscanf(target_out, "%ZX", message);
+  gmp_fscanf(target_out, "%ZX", message.get_mpz_t());
   interactionCount++;
 }
 
-/**
- * @brief Function which ads 20 more power trace samples.
- */
 void Attack::AddMoreSamples(){
-  powerTraces.resize(sampleCount + 100);
-  messages.resize(sampleCount + 100);
-  powerTracesPhase2.resize(sampleCount + 100);
-  for(int i = sampleCount; i < sampleCount + 100; i++) {
-    mpz_class sector(i); 
+  powerTraces.resize(sampleCount + minSample);
+  tweaks.resize(sampleCount + minSample);
+  for(int i = sampleCount; i < sampleCount + minSample; i++) {
+    mpz_class sector = randomGenerator.get_z_bits(128);
+    tweaks[i] = sector; 
     mpz_class message;
     vector<int> currentTrace;
     vector<int> currentTracePhase2;
     Interact(0, sector, message, currentTrace, currentTracePhase2);
     powerTraces[i] = currentTrace;
-    powerTracesPhase2[i] = currentTracePhase2;
-    messages[i] = message;
   }
-  sampleCount += 100;
+  sampleCount += minSample;
+}
+
+void Attack::SamplePart2() {
+  powerTracesPhase2.resize(sampleCountPhase2 + minSample);
+  messages.resize(sampleCountPhase2 + minSample);
+  messagesPhase2.resize(sampleCountPhase2 + minSample);
+  sectorsPhase2.resize(sampleCountPhase2 + minSample);
+  for(int i = sampleCountPhase2; i < sampleCountPhase2 + minSample; i++) {
+      mpz_class sector = randomGenerator.get_z_range(maxSector);
+      sectorsPhase2[i] = sector;
+      mpz_class message;
+      vector<int> currentTrace;
+      vector<int> currentTracePhase2;
+      Interact(0, sector, message, currentTrace, currentTracePhase2);
+      powerTracesPhase2[i] = currentTracePhase2;
+      messages[i] = message;
+  }
+  sampleCountPhase2 += minSample;
 }
 
 float Attack::Mean(vector<int> trace) {
   float sum = 0;
-  for(auto t : trace) {
-    sum += t;
+  for(int t : trace) {
+    sum += (float)t;
   }
   if(trace.size() != 0) {
-    return (float) sum / trace.size();
+    return ((float) sum) / trace.size();
   } else {
     return 0;
   }
 }
 
-float Attack::PearsonCorr(vector<int> x, vector<int> y) {
-  float cov_xy(0), sum_x(0), sum_y(0);
-  float meanX = Mean(x);
-  float meanY = Mean(y);
+double Attack::PearsonCorr(vector<int> x, vector<int> y){
+  double xMean=0, yMean=0;
 
-  for (int i = 0; i < x.size(); i++) {
-    cov_xy += (x[i] - meanX) * (y[i] - meanY);
-    sum_x  += (x[i] - meanX) * (x[i] - meanX);    
-    sum_y  += (y[i] - meanY) * (y[i] - meanY);
+  // compute mean of x
+  for(int i=0; i<sampleCount; i++){
+    xMean += x[i];
+    yMean += y[i];
   }
-  
-  return (float) cov_xy / (sqrt(sum_x) * sqrt(sum_y));
+  xMean = xMean/sampleCount;
+  yMean = yMean/sampleCount;
+
+  double top=0;
+  double sx = 0, sy = 0, sx2 = 0, sy2 = 0;
+  for(int i=0; i<sampleCount; i++){
+    sx = (x[i]-xMean);
+    sy = (y[i]-yMean);
+    top += sx * sy;
+    sx2 += sx*sx;
+    sy2 += sy*sy;
+  }
+  double check = sqrt(sx2*sy2);
+  if(check != 0 )
+    return top/check;
+  else{
+    return 0;
+  }
 }
 
-/**
- * @brief Driver function which performs all the steps of the attack and prints any relevant output to stdout or sterr.
- */
 void Attack::Execute() {
-  AddMoreSamples();
-  Phase1();
-  Phase2();
-}
-
-vector<unsigned char> Attack::Phase2() {
-  int minLen = powerTracesPhase2[0].size();
-  for(int i = 1; i < powerTracesPhase2.size(); i++) {
-    if(powerTracesPhase2[i].size() < minLen) {
-      minLen = powerTracesPhase2[i].size();
-    }
-  }
-  vector<vector<int>> powerMatrix(minLen, vector<int> (powerTracesPhase2.size()));
-  for(int i = 0; i < minLen; i++) {
-    for(int j = 0; j < powerTracesPhase2.size(); j++) {
-      powerMatrix[i][j] = powerTracesPhase2[j][i];
-    }
-  }
-
-  vector<unsigned char> key1(16);
-  
-  //recover each byte of the key individually
-  #pragma omp parallel for
-  for(int n = 0; n < 16; n++) {
-    vector<vector<int>> simulatedTraces(256, vector<int>(messagesPhase2.size()));
-    for(int i = 0; i < messagesPhase2.size(); i++) {
-      mpz_class msg = messagesPhase2[i] >> (8 * n);
-      //get the byte from the message which corresponds to the key byte we are recovering
-      int byte = msg.get_si() & 0xff;
-      for(int k = 0; k < 256; k++) {
-        simulatedTraces[k][i] = HammingWeight[SubBytes[byte ^ k]];
+  vector<unsigned char> key1, key2;
+  int attempts = 0;
+  do{
+    AddMoreSamples();
+    key2 = Phase1();
+    sampleCountPhase2 = 0;
+    int tries = 5;
+    do{
+      SamplePart2();
+      cout<<sampleCountPhase2<<endl;
+      for(int i = 0; i < messages.size(); i++) {
+        mpz_class sector = sectorsPhase2[i];
+        unsigned char m_char[16] = {0}, t[16] = {0};
+        mpz_export(m_char, NULL, 1, 1, 1, 0, sector.get_mpz_t());
+        AES_KEY rk;
+        AES_set_encrypt_key(key2.data(), 128, &rk);
+        AES_encrypt(m_char, t, &rk);
+        mpz_class T;
+        mpz_import(T.get_mpz_t(), 16, 1, 1, 1, 0, t);      
+        messagesPhase2[i] = T ^ messages[i];
       }
-    }
+      key1 = Phase2();
+      tries--;
+    } while(!TestKey(key1, key2) && tries > 0);
 
-    //calculate correlation between trace samples and our model
-    vector<vector<float>>correlation(256, vector<float> (powerMatrix.size()));
-    for(int i = 0; i < 256; i++) {
-      for(int j = 0; j < powerMatrix.size(); j++) {
-        if(powerMatrix[j].size() == simulatedTraces[i].size()) {
-          correlation[i][j] = PearsonCorr(simulatedTraces[i], powerMatrix[j]);
-        }
-      }
-    }
-
-    float maxCorr = -2;
-    int bestByte = -1;
-    for(int i = 0; i < 256; i++) {
-      for(int j = 0; j < powerMatrix.size(); j++) {
-        if(abs(correlation[i][j]) > maxCorr) {
-          maxCorr = abs(correlation[i][j]);
-          bestByte = i;
-        }
-      }
-    }
-    key1[n] = bestByte;
-  }
-  for(int n = 15; n >= 0; n--){
-    printf("%02X", (int)key1[n]);
-  }
-  cout<<endl;
-  return key1;
+    attempts ++;
+    gmp_printf("Resetting\n\n");
+  } while(attempts < 10);
+  gmp_printf("FAILED\n");
 }
 
 vector<unsigned char> Attack::Phase1() {
-  int minLen = powerTraces[0].size();
-  for(int i = 1; i < powerTraces.size(); i++) {
-    if(powerTraces[i].size() < minLen) {
-      minLen = powerTraces[i].size();
-    }
-  }
-  vector<vector<int>> powerMatrix(minLen, vector<int> (powerTraces.size()));
-  for(int i = 0; i < minLen; i++) {
-    for(int j = 0; j < powerTraces.size(); j++) {
+  vector<vector<int>> powerMatrix(traceSize, vector<int> (sampleCount));
+  for(int i = 0; i < traceSize; i++) {
+    for(int j = 0; j < sampleCount; j++) {
       powerMatrix[i][j] = powerTraces[j][i];
     }
   }
-
   vector<unsigned char> key2(16);
   
   //recover each byte of the key individually
   #pragma omp parallel for
   for(int n = 0; n < 16; n++) {
-    vector<vector<int>> simulatedTraces(256, vector<int>(messages.size()));
-    for(int i = 0; i < messages.size(); i++) {
-      mpz_class msg = messages[i] >> (8 * n);
+    vector<vector<int>> simulatedTraces(256, vector<int>(sampleCount));
+    for(int i = 0; i < sampleCount; i++) {
+      mpz_class msg = tweaks[i] >> (8 * n);
       //get the byte from the message which corresponds to the key byte we are recovering
       int byte = msg.get_si() & 0xff;
       for(int k = 0; k < 256; k++) {
@@ -236,53 +221,121 @@ vector<unsigned char> Attack::Phase1() {
     }
 
     //calculate correlation between trace samples and our model
-    vector<vector<float>>correlation(256, vector<float> (powerMatrix.size()));
+    vector<vector<double>>correlation(256, vector<double> (traceSize));
     for(int i = 0; i < 256; i++) {
-      for(int j = 0; j < powerMatrix.size(); j++) {
-        if(powerMatrix[j].size() == simulatedTraces[i].size()) {
-          correlation[i][j] = PearsonCorr(simulatedTraces[i], powerMatrix[j]);
-        }
+      for(int j = 0; j < traceSize; j++) {
+        correlation[i][j] = PearsonCorr(simulatedTraces[i], powerMatrix[j]);
       }
     }
 
-    float maxCorr = -2;
+    double maxCorr = -2;
     int bestByte = -1;
     for(int i = 0; i < 256; i++) {
-      for(int j = 0; j < powerMatrix.size(); j++) {
-        if(abs(correlation[i][j]) > maxCorr) {
-          maxCorr = abs(correlation[i][j]);
+      for(int j = 0; j < traceSize; j++) {
+        if(correlation[i][j] > maxCorr) {
+          maxCorr = correlation[i][j];
           bestByte = i;
         }
       }
     }
-    key2[n] = bestByte;
+    key2[15 - n] = bestByte;
   }
-  for(int n = 15; n >= 0; n--){
+  printf("Value of Key 2 is:");
+  for(int n = 0; n < 16; n++){
     printf("%02X", (int)key2[n]);
   }
-  cout<<endl;
+  printf("\n");
 
-  messagesPhase2.resize(messages.size());
-  for(int i = 0; i < messages.size(); i++) {
-    mpz_class sector(i);
-    unsigned char m_char[16] = {0}, t[16] = {0};
-    mpz_export(m_char, NULL, 1, 1, 0, 0, sector.get_mpz_t());
-    AES_KEY rk;
-    AES_set_encrypt_key(key2.data(), 128, &rk);
-    AES_encrypt(m_char, t, &rk);
-    mpz_class nextPlaintext;
-    mpz_import(nextPlaintext.get_mpz_t(), 16, 1, 1, 0, 0, t);      
-    messagesPhase2[i] = nextPlaintext;
-    i++;
-  }
   return key2;
 }
 
-/**
- * @brief Function which aborts execution and prints an error message to stderr.
- *
- * @param errorMessage  the error message to be printed to stderr
- */
+vector<unsigned char> Attack::Phase2(){
+  vector<vector<int>> powerMatrix(traceSize, vector<int> (sampleCountPhase2));
+  for(int i = 0; i < traceSize; i++) {
+    for(int j = 0; j < sampleCountPhase2; j++) {
+      powerMatrix[i][j] = powerTracesPhase2[j][i];
+    }
+  }
+  vector<unsigned char> key1(16);
+  
+  //recover each byte of the key individually
+  #pragma omp parallel for
+  for(int n = 0; n < 16; n++) {
+    vector<vector<int>> simulatedTraces(256, vector<int>(sampleCountPhase2));
+    for(int i = 0; i < sampleCountPhase2; i++) {
+      mpz_class msg = messagesPhase2[i] >> (8 * n);
+      //get the byte from the message which corresponds to the key byte we are recovering
+      int byte = msg.get_si() & 0xff;
+      for(int k = 0; k < 256; k++) {
+        simulatedTraces[k][i] = HammingWeight[InvSubBytes[byte] ^ k];
+      }
+    }
+
+    //calculate correlation between trace samples and our model
+    vector<vector<double>>correlation(256, vector<double> (traceSize));
+    for(int i = 0; i < 256; i++) {
+      for(int j = 0; j < traceSize; j++) {
+        correlation[i][j] = PearsonCorr(simulatedTraces[i], powerMatrix[j]);
+      }
+    }
+
+    double maxCorr = -2;
+    int bestByte = -1;
+    for(int i = 0; i < 256; i++) {
+      for(int j = 0; j < traceSize; j++) {
+        if(correlation[i][j] > maxCorr) {
+          maxCorr = correlation[i][j];
+          bestByte = i;
+        }
+      }
+    }
+    key1[15 - n] = bestByte;
+  }
+  printf("Value of Key 1 is:");
+  for(int n = 0; n < 16; n++){
+    printf("%02X", (int)key1[n]);
+  }
+  printf("\n");
+  return key1;
+}
+
+bool Attack::TestKey(vector<unsigned char> key1, vector<unsigned char> key2) {
+  mpz_class sector(167771111002177);
+  mpz_class message;
+  mpz_class tweakedMessage;
+  vector<int> currentTrace;
+  vector<int> currentTracePhase2;
+  Interact(0, sector, message, currentTrace, currentTracePhase2);
+
+  unsigned char m_char[16] = {0}, t[16] = {0};
+  unsigned char rev_m_char[16] = {0};
+  mpz_export(m_char, NULL, 1, 1, 0, 0, sector.get_mpz_t());
+  for(int i = 0 ; i <16; i++){
+    rev_m_char[15 - i] = m_char[i];
+  }
+
+  printf("\n");
+  AES_KEY rk;
+  AES_set_encrypt_key(key2.data(), 128, &rk);
+  AES_encrypt(rev_m_char, t, &rk);
+     
+  unsigned char res[16] = {0};
+  AES_KEY sk;
+  AES_set_encrypt_key(key1.data(), 128, &sk);
+  AES_decrypt(t, res, &sk);
+
+  mpz_class PP, T;
+  mpz_import(PP.get_mpz_t(), 16, 1, 1, 1, 0, res);
+  mpz_import(T.get_mpz_t(), 16, 1, 1, 1, 0, t);    
+  mpz_class P = PP ^ T;
+  if( P == message){
+    return true;
+  } else {
+    gmp_printf("%ZX\n%ZX\n\n",P,message);
+    return false;
+  }
+}
+
 void Attack::throwErrorAndAbort(string errorMessage) {
   cerr<<errorMessage<<endl;
   cleanup(-1);
